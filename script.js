6bc7d4a56e5ce49f12e67212aaf9b31498261c65
Roy Tao <148505011+RuiyiTao@users.noreply.github.com>
2024-11-05 00:26:17 +0000

    spinning nerd

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.body.addEventListener("mousemove", (event) => {
-    document.querySelector("img").style.x = event.clientX;
-    document.querySelector("img").style.y = event.clientY;
+var time = 0;
+var imagepositionx = 0;
+var imagepositiony = 0;
+document.addEventListener("mousemove", (event) => {
+    imagepositionx = event.clientX;
+    imagepositiony = event.clientY;
 });
+
+setInterval(() => {
+    document.getElementById("image").style.left = (imagepositionx - 125 + Math.cos(time) * 200) + "px";
+    document.getElementById("image").style.top = (imagepositiony - 125 + Math.sin(time) * 200) + "px";
+    time += 0.2;
+}, 10);
